96744586d680393e9b5bb737a4881f6304335c39
clara wolff <cwo@systemli.org>
2020-12-09 15:53:30 +0100

    removed controllers and routes to substitute them by API controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :changes, only: :new
-  resources :locks, only: [:create, :update] do
-    resources :changes, only: :create
-  end
+
 end
